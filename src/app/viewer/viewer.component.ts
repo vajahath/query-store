702b58f20df2278b21f03aef5f97c5c8d114362b
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-12-30 16:34:38 +0530

    base

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QUERIES } from '../queries.mock';
 
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
-export class AppViewerComponent {}
+export class AppViewerComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
