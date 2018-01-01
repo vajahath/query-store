767c35858c1088a34241a079323e2e50ea1cb5c6
Vajahath Ahmed <vajuoff.1@gmail.com>
2018-01-01 11:49:54 +0530

    initial attempt to spinner ready

--- src/app/Query.class.ts
+++ src/app/Query.class.ts
@@ -3,21 +3,28 @@ export class Query {
   query: string;
   title: string;
   created_by: string;
-  finished: boolean;
   created_at: string;
+
+  finished: boolean;
+  finishedInProgress?: boolean;
 
   ready_for_vajahath: boolean;
   vajahath_status: 'passed' | 'pending' | 'failed' | 'inactive';
+  ready_for_vajahathInProgress?: boolean;
 
   ready_for_nithin: boolean;
   nithin_status: 'passed' | 'pending' | 'failed' | 'inactive';
+  ready_for_nithinInProgress?: boolean;
 
   ready_for_local: boolean;
   local_status: 'passed' | 'pending' | 'failed' | 'inactive';
+  ready_for_localInProgress?: boolean;
 
   ready_for_staging: boolean;
   staging_status: 'passed' | 'pending' | 'failed' | 'inactive';
+  ready_for_stagingInProgress?: boolean;
 
   ready_for_production: boolean;
   production_status: 'passed' | 'pending' | 'failed' | 'inactive';
+  ready_for_productionInProgress?: boolean;
 }
