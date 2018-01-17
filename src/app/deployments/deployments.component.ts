4fd29e565f49c11d06c57644bd1885b00dcc4fdb
Vajahath Ahmed <vajuoff.1@gmail.com>
2018-01-17 20:29:44 +0530

    deployment component: initial attempt

--- src/app/deployments/deployments.component.ts
+++ src/app/deployments/deployments.component.ts
@@ -13,20 +13,36 @@ import { Query } from '../Query.class';
 })
 export class DeploymentsComponent implements OnInit {
   queries: Query[];
+  env: string;
+  deployInProgress: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private queryService: QueryService,
     private location: Location
-  ) {}
+  ) {
+    this.deployInProgress = false;
+    this.env = this.route.snapshot.paramMap.get('env');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getQueries();
+  }
 
   getQueries(): void {
-    const env = this.route.snapshot.paramMap.get('env');
+    console.log(`getting queries on env ${this.env}`);
 
     this.queryService
       .getQueries()
       .subscribe(queries => (this.queries = queries));
+  }
+
+  deploy() {
+    this.deployInProgress = true;
+    setTimeout(() => {
+      console.log('deploy clicked');
+      this.deployInProgress = false;
+      this.getQueries();
+    }, 1000);
   }
 }
